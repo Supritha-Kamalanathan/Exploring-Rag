52f6cc1180303ad15abe141c7b2048b3aa532b00
Supritha <supritha.kamal@gmail.com>
2024-10-06 19:13:53 +0530

    initial commit: chat with pdf

---
+++ mini_projects/chat_with_pdf/main.py
@@ -0,0 +1,104 @@
+import streamlit as st
+from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from sentence_transformers import SentenceTransformer
+import numpy as np
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+
+load_dotenv()
+
+gpt_model = GPT2LMHeadModel.from_pretrained('gpt2')
+gpt_tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+
+embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def format_text(text):
+    formatted_text = text.replace("\n", ' ')
+    return formatted_text
+
+def get_texts_info(pdf, chunk_size=1000, chunk_overlap=200):
+    texts_info = []
+    if pdf is not None:
+        content = PdfReader(pdf)
+
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len
+        )
+
+        for page_no, page in enumerate(content.pages):
+            text = format_text(page.extract_text())
+            chunks = splitter.split_text(text)
+            texts_info.append({
+                "page_number": page_no,
+                "page_char_count": len(text),
+                "page_word_count": len(text.split()),
+                "page_sentence_count": len(text.split(". ")),
+                "page_token_count": len(text) / 4,
+                "text": text,
+                "chunks": chunks,
+                "num_chunks": len(chunks)
+            })
+    return texts_info
+
+def calculate_embeddings(texts_info):
+    chunks = []
+    embeddings = []
+    
+    for i in texts_info:
+        for chunk in i["chunks"]:
+            chunks.append(chunk)
+            embedding = embedding_model.encode(chunk) 
+            embeddings.append(embedding)
+    
+    return chunks, np.array(embeddings)
+
+def generate_response(retrieved_docs, query):
+    text = " ".join(retrieved_docs)
+
+    prompt = f"""Context: {text}\n\nBased on the context, please answer the following question: {query}\n\nAnswer:"""
+
+    inputs = gpt_tokenizer.encode(prompt, return_tensors="pt")
+    outputs = gpt_model.generate(inputs, max_length=800, num_return_sequences=1, do_sample=True, top_p=0.95, top_k=50)
+
+    response = gpt_tokenizer.decode(outputs[0], skip_special_tokens=True)
+    response = response.replace(prompt, "").strip()
+
+    return response
+
+def main():
+    st.set_page_config(
+        page_title="Chat with your PDF",
+        page_icon="🗪"
+    )
+
+    st.header("Chat with your PDF's 🗪")
+
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    if pdf:
+        query = st.text_input("Your question: ")
+
+        if query:
+            with st.spinner("Getting texts info..."):
+                texts_info = get_texts_info(pdf)
+            with st.spinner("Calculating embeddings..."):
+                chunks, embeddings = calculate_embeddings(texts_info)
+
+            query_embedding = embedding_model.encode(query)
+
+            with st.spinner("Retrieving relevant docs..."):
+                similarities = cosine_similarity(query_embedding.reshape(1, -1), embeddings)[0]
+                top_indices = similarities.argsort()[-3:][::-1] 
+                retrieved_docs = [chunks[i] for i in top_indices]
+
+            with st.spinner("Generating response..."):
+                response = generate_response(retrieved_docs, query)
+
+            st.write(response)
+
+if __name__ == "__main__":
+    main()
